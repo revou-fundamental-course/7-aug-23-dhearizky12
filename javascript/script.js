08761b8a7f1065cd2a24246585cfa334f8038ad1
dhea rizky <dhea.rizky@formulatrix.com>
2023-08-18 17:16:07 +0700

    add logic and UI for kelilingSegitiga

--- javascript/script.js
+++ javascript/script.js
@@ -1,42 +1,53 @@
 const hitungLuas = document.getElementById("hitungLuas");
-hitungLuas.addEventListener("click",function(){
+hitungLuas.addEventListener("click", function () {
     const alasLuas = document.getElementById("alasLuas");
     const nilaiAlas = alasLuas.value;
 
     const tinggiLuas = document.getElementById("tinggiLuas");
     const nilaiTinggi = tinggiLuas.value;
 
-    const hasil = (1/2)*nilaiAlas*nilaiTinggi;
-    // const hasilLuas = document.getElementById("hasilLuas");
-    // hasilLuas.innerHTML=
-    // "L= 1/2 x a x t " +
-    // "<br />"+
-    // "L = 1/2 x " + nilaiAlas +"x " + nilaiTinggi + "<br />" + "L = "+ hasil;
+    const hasil = (1 / 2) * nilaiAlas * nilaiTinggi;
+
     const formulaValueElements = document.querySelectorAll(".formula-value");
-    formulaValueElements[0].textContent=nilaiAlas;
-    formulaValueElements[1].textContent=nilaiTinggi;
-    formulaValueElements[2].textContent=hasil;
+    formulaValueElements[0].textContent = nilaiAlas;
+    formulaValueElements[1].textContent = nilaiTinggi;
+    formulaValueElements[2].textContent = hasil;
 });
 
 const resetLuas = document.getElementById("resetLuas");
-resetLuas.addEventListener("click",function(){
+resetLuas.addEventListener("click", function () {
     const hasilLuas = document.getElementById("hasilLuas");
-    const formulaValueElements=document.querySelectorAll(".formula-value");
+    const formulaValueElements = document.querySelectorAll(".formula-value");
 
-    formulaValueElements[0].textContent="";
-    formulaValueElements[1].textContent="";
-    formulaValueElements[2].textContent="";
+    formulaValueElements[0].textContent = "";
+    formulaValueElements[1].textContent = "";
+    formulaValueElements[2].textContent = "";
+});
 
-    const formulaExpressionElements = document.querySelectorAll(".formula-expression");
- 
-    formulaExpressionElements[2].textContent = "x";
-    formulaExpressionElements[3].textContent = "";
-    formulaExpressionElements[4].textContent = "x";
-    formulaExpressionElements[5].textContent = "";
-    
-    // Menghilangkan tampilan hasil
-    hasilLuas.style.display = "none";
+const hitungKeliling = document.getElementById("hitungKeliling");
+hitungKeliling.addEventListener("click", function () {
+    const s1 = parseFloat(document.getElementById("s1").value);
+    const s2 = parseFloat(document.getElementById("s2").value);
+    const s3 = parseFloat(document.getElementById("s3").value);
 
+    const hasilKeliling = s1 + s2 + s3;
+
+    const formulaValueElements = document.querySelectorAll(".formula-value-keliling");
+    formulaValueElements[0].textContent = s1;
+    formulaValueElements[1].textContent = s2;
+    formulaValueElements[2].textContent = s3;
+    formulaValueElements[3].textContent = hasilKeliling;
+});
+
+const resetKeliling = document.getElementById("resetKeliling");
+resetKeliling.addEventListener("click", function () {
+   
+    const formulaValueElements = document.querySelectorAll(".formula-value-keliling");
+
+    formulaValueElements[0].textContent = "";
+    formulaValueElements[1].textContent = "";
+    formulaValueElements[2].textContent = "";
+    formulaValueElements[3].textContent = "";
 
 });
 
